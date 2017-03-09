753923c621b2aa8bc9cb247abd6ca2fa8b516acd
louisdubuc95 <test@hotmail.ca>
2017-03-09 17:48:45 -0500

    fleure mario

--- Projet_IFT3100/src/renderer.cpp
+++ Projet_IFT3100/src/renderer.cpp
@@ -122,6 +122,39 @@ void Renderer::draw()
 		ofSetColor(0, 255, 0);
 		ofDrawTriangle(tx1, ty1, tx2, ty2, tx3, ty3);
 	}
+	int x = -600;
+	int y =-600; 
+	
+
+	ofSetColor(0, 0, 0);
+	ofDrawEllipse(x - 35, y, 30, 75);
+
+	ofSetColor(0, 0, 0);
+	ofDrawEllipse(x + 35, y, 30, 75);
+
+	ofSetColor(255, 255, 255);
+	ofDrawEllipse(x, y, 250, 100);
+
+
+	ofSetColor(255, 204, 0);
+	ofDrawEllipse(x, y, 325, 150);
+
+	ofSetColor(255,153,2);
+	ofDrawEllipse(x , y , 400, 200);
+
+	ofSetColor(51, 204, 0);
+	ofDrawRectangle(x+45, y, -90, -250);
+
+	ofDrawTriangle(x+45,y-250,x + 85, y - 150 ,x + 200, y-130);
+	ofDrawTriangle(x + 45, y - 250, x + 85 + 60, y - 230, x + 200, y - 130);
+
+	ofDrawTriangle(x - 45, y - 250, x - 85, y - 150, x - 200, y - 130);
+	ofDrawTriangle(x - 45, y - 250, x - 85 - 60, y - 230, x - 200, y - 130);
+	
+
+
+
+
 
 	camera->end();
 }
