66ef3edaaa2d13490aabb4e7df5c096f8cf4ac2e
William <william.duquet@videotron.ca>
2017-03-12 11:55:07 -0400

    Réorganisation des tous les .h

--- Projet_IFT3100/src/gui.h
+++ Projet_IFT3100/src/gui.h
@@ -5,8 +5,7 @@
 
 class Gui : public ofBaseApp {
 public:
-	int offsetX;
-
+	// Image //////////////////////////////////////////////////////////////////////////////////////
 	// Initialisation des paramètres par défaut de l'image
 	int defaultPosX;
 	int defaultPosY;
@@ -17,23 +16,79 @@ public:
 	int defaultBrightnessImage;
 	int defaultAlphaImage;
 
-	int defaultCircleRadius;
-	float defaultFov;
-	int defaultCameraNear;
-	int defaultCameraFar;
+	// Initialisation des sliders des paramètres de l'image
+	ofxPanel guiImage;
+	ofxButton exportButton; bool exportCheck;
+	ofxButton importButton; bool importCheck;
+	ofxIntSlider posImageX;
+	ofxIntSlider posImageY;
+	ofxIntSlider sizeImageWidth;
+	ofxIntSlider sizeImageHeight;
+	ofxIntSlider hueImage;
+	ofxIntSlider saturationImage;
+	ofxIntSlider brightnessImage;
+	ofxIntSlider alphaImage;
 
-	Gui();
+	int getPosImageX();
+	int getPosImageY();
+	int getSizeImageWidth();
+	int getSizeImageHeight();
+	int getHueImage();
+	int getSaturationImage();
+	int getBrightnessImage();
+	int getAlphaImage();
 
-	void setup();
-	void reset();
-	void update();
-	void draw();
+	// Dessin vectoriel ///////////////////////////////////////////////////////////////////////////
+	//Primitive 2d
+	//Éléments pour les primitives vectorielles
+	ofxPanel guiPrimitive;
+	ofxFloatSlider posXSlider;
+	ofxFloatSlider posYSlider;
+	ofxFloatSlider tx1;
+	ofxFloatSlider tx2;
+	ofxFloatSlider ty1;
+	ofxFloatSlider ty2;
+	ofxFloatSlider tz1;
+	ofxFloatSlider tz2;
+	ofxFloatSlider posXEllipseSlider;
+	ofxFloatSlider posYEllipseSlider;
 
+	ofxFloatSlider positionYEllipseSlider;
+	ofxFloatSlider positionXEllipseSlider;
+	ofxFloatSlider positionZEllipseSlider;
 
-	float posX;
-	float posY;
+	ofxFloatSlider positionXRectangleSlider;
+	ofxFloatSlider positionYRectangleSlider;
+	ofxFloatSlider positionZRectangleSlider;
 
-	float getTX1(); 
+	ofxPanel guiColorPrimitives;
+	ofxIntSlider sliderRStroke;
+	ofxIntSlider sliderGStroke;
+	ofxIntSlider sliderBStroke;
+	ofxIntSlider sliderRFill;
+	ofxIntSlider sliderGFill;
+	ofxIntSlider sliderBFill;
+
+	ofxIntSlider sliderFONDR;
+	ofxIntSlider sliderFONDG;
+	ofxIntSlider sliderFONDB;
+
+	ofxIntSlider sliderContour;
+
+	float getPositionXRectangleSlider();
+	float getPositionYRectangleSlider();
+	float getPositionZRectangleSlider();
+
+	float getPositionXEllipseSlider();
+	float getPositionYEllipseSlider();
+	float getPositionZEllipseSlider();
+
+	bool getToggleRectangle();
+	bool getToggleEllipse();
+	bool getToggleLigne();
+	bool getToggleFormeVectorielle();
+
+	float getTX1();
 	float getTX2();
 	float getTY1();
 	float getTY2();
@@ -53,145 +108,13 @@ public:
 
 	int getContour();
 
-	float getposXEllipseSlider(); 
-	float getposYEllipseSlider();	
-
+	float getposXEllipseSlider();
+	float getposYEllipseSlider();
 
 	float getPosXSlider();
 	float getPosYSlider();
-	
-	//forme vectorielle 
-	float getformeVectorielleRSlider(); 
-	float getformeVectorielleTSlider(); 
-	float getformeVectoriellePSlider();
 
-	float getRotAngle();
-	int getRotX();
-	int getRotY();
-	int getRotZ();
-
-	//Primitive 2d
-	float getPositionXRectangleSlider();
-	float getPositionYRectangleSlider(); 
-	float getPositionZRectangleSlider();
-
-	float getPositionXEllipseSlider(); 
-	float getPositionYEllipseSlider(); 
-	float getPositionZEllipseSlider();
-
-	
-	bool getToggleRectangle();
-	bool getToggleEllipse();
-	bool getToggleLigne();
-	bool getToggleFormeVectorielle();
-
-	int getPosImageX();
-	int getPosImageY();
-	int getSizeImageWidth();
-	int getSizeImageHeight();
-	int getHueImage();
-	int getSaturationImage();
-	int getBrightnessImage();
-	int getAlphaImage();
-
-	int getXLight();
-	int getYLight();
-	int getZLight();
-
-	int getRadius();
-	float getFov();
-	int getCameraNear();
-	int getCameraFar();
-
-	// Initialisation des fonctions de l'application
-	ofxPanel guiOption;
-	ofxButton exitButton;
-
-	// Initialisation des sliders des paramètres de l'image
-	ofxPanel guiImage;
-	ofxButton exportButton; bool exportCheck;
-	ofxButton importButton; bool importCheck;
-	ofxIntSlider posImageX;
-	ofxIntSlider posImageY;
-	ofxIntSlider sizeImageWidth;
-	ofxIntSlider sizeImageHeight;
-	ofxIntSlider hueImage;
-	ofxIntSlider saturationImage;
-	ofxIntSlider brightnessImage;
-	ofxIntSlider alphaImage;
-
-	//Primitive 3D
-	ofxPanel guiPrimitive3d;
-	ofxToggle primitive3dToggle;
-	ofxToggle primitive3dStroke; 
-	ofxToggle primitive3dRotation;
-	ofxFloatSlider positionPrimitive3dX; 
-	ofxFloatSlider positionPrimitive3dY;
-	ofxFloatSlider positionPrimitive3dZ;
-	ofxFloatSlider Primitive3dSize; 
-	
-	float getpositionPrimitive3dX();
-	float getpositionPrimitive3dY();
-	float getpositionPrimitive3dZ();
-	float getprimitive3dSize();
-	bool getToggleprimitive3d(); 
-	bool getToggleprimitive3dStroke();
-	bool getToggleprimitive3dRotation();
-
-	ofxToggle primitive3dToggleBox;
-	ofxToggle primitive3dStrokeBox;
-	ofxToggle primitive3dRotationBox;
-	ofxFloatSlider positionPrimitive3dXBox;
-	ofxFloatSlider positionPrimitive3dYBox;
-	ofxFloatSlider positionPrimitive3dZBox;
-	ofxFloatSlider Primitive3dSizeBox;
-
-	float getpositionPrimitive3dXBox();
-	float getpositionPrimitive3dYBox();
-	float getpositionPrimitive3dZBox();
-	float getprimitive3dSizeBox();
-	bool getToggleprimitive3dBox();
-	bool getToggleprimitive3dStrokeBox();
-	bool getToggleprimitive3dRotationBox();
-
-
-	
-
-
-	
-
-	// Initialisation des paramètres du cercle
-	ofxPanel guiCircle;
-	ofxIntSlider circleRadius;
-
-	// Initialisation des paramètres de la caméra
-	ofxPanel guiCamera;
-	ofxFloatSlider fovSlider;
-	ofxIntSlider cameraNearSlider;
-	ofxIntSlider cameraFarSlider;
-
-	//Éléments pour les primitives vectorielles
-	ofxPanel guiPrimitive;
-	ofxFloatSlider posXSlider;
-	ofxFloatSlider posYSlider;
-	ofxFloatSlider tx1;
-	ofxFloatSlider tx2;
-	ofxFloatSlider ty1;
-	ofxFloatSlider ty2;
-	ofxFloatSlider tz1;
-	ofxFloatSlider tz2;
-	ofxFloatSlider posXEllipseSlider;
-	ofxFloatSlider posYEllipseSlider;
-
-	ofxFloatSlider positionYEllipseSlider;
-	ofxFloatSlider positionXEllipseSlider; 
-	ofxFloatSlider positionZEllipseSlider;
-
-	ofxFloatSlider positionXRectangleSlider; 
-	ofxFloatSlider positionYRectangleSlider;
-	ofxFloatSlider positionZRectangleSlider;
-
-
+	// Transformation /////////////////////////////////////////////////////////////////////////////
 	// Forme vectorielle
 	ofxFloatSlider formeVectorielleRSlider;
 	ofxFloatSlider formeVectorielleTSlider;
@@ -202,37 +125,108 @@ public:
 	ofxIntSlider rotYSlider;
 	ofxIntSlider rotZSlider;
 
-	ofxPanel guiColorPrimitives;
-	ofxIntSlider sliderRStroke;
-	ofxIntSlider sliderGStroke;
-	ofxIntSlider sliderBStroke;
-	ofxIntSlider sliderRFill;
-	ofxIntSlider sliderGFill;
-	ofxIntSlider sliderBFill;
+	float getformeVectorielleRSlider();
+	float getformeVectorielleTSlider();
+	float getformeVectoriellePSlider();
 
-	ofxIntSlider sliderFONDR;
-	ofxIntSlider sliderFONDG;
-	ofxIntSlider sliderFONDB;
+	float getRotAngle();
+	int getRotX();
+	int getRotY();
+	int getRotZ();
 
-	ofxIntSlider sliderContour;
+	// Géométrie //////////////////////////////////////////////////////////////////////////////////
+	// Initialisation des paramètres du cercle
+	int defaultCircleRadius;
+
+	ofxPanel guiCircle;
+	ofxIntSlider circleRadius;
+
+	int getRadius();
+
+	//Primitive 3D
+	ofxPanel guiPrimitive3d;
+	ofxToggle primitive3dToggle;
+	ofxToggle primitive3dStroke;
+	ofxToggle primitive3dRotation;
+	ofxFloatSlider positionPrimitive3dX;
+	ofxFloatSlider positionPrimitive3dY;
+	ofxFloatSlider positionPrimitive3dZ;
+	ofxFloatSlider Primitive3dSize;
+
+	ofxToggle primitive3dToggleBox;
+	ofxToggle primitive3dStrokeBox;
+	ofxToggle primitive3dRotationBox;
+	ofxFloatSlider positionPrimitive3dXBox;
+	ofxFloatSlider positionPrimitive3dYBox;
+	ofxFloatSlider positionPrimitive3dZBox;
+	ofxFloatSlider Primitive3dSizeBox;
+
+	float getpositionPrimitive3dX();
+	float getpositionPrimitive3dY();
+	float getpositionPrimitive3dZ();
+	float getprimitive3dSize();
+	bool getToggleprimitive3d();
+	bool getToggleprimitive3dStroke();
+	bool getToggleprimitive3dRotation();
+
+	float getpositionPrimitive3dXBox();
+	float getpositionPrimitive3dYBox();
+	float getpositionPrimitive3dZBox();
+	float getprimitive3dSizeBox();
+	bool getToggleprimitive3dBox();
+	bool getToggleprimitive3dStrokeBox();
+	bool getToggleprimitive3dRotationBox();
 
 	// Scène 3D
 	ofxPanel guiScene;
 	ofxButton modelButton; bool modelCheck;
 
+	// Caméra /////////////////////////////////////////////////////////////////////////////////////
+	float defaultFov;
+	int defaultCameraNear;
+	int defaultCameraFar;
+
+	// Initialisation des paramètres de la caméra
+	ofxPanel guiCamera;
+	ofxFloatSlider fovSlider;
+	ofxIntSlider cameraNearSlider;
+	ofxIntSlider cameraFarSlider;
+
+	float getFov();
+	int getCameraNear();
+	int getCameraFar();
+
+	// Autres /////////////////////////////////////////////////////////////////////////////////////
+	int offsetX;
 	// Lumière
 	ofxPanel guiLight;
 	ofxIntSlider xLightSlider;
 	ofxIntSlider yLightSlider;
 	ofxIntSlider zLightSlider;
-
-	//Éléments Autres
+	int getXLight();
+	int getYLight();
+	int getZLight();
+	// Éléments Autres
 	ofxPanel guiOptions;
 	ofxToggle rec;
 	ofxToggle ell;
 	ofxToggle lig;
 	ofxToggle formeVec;
+	// Initialisation des fonctions de l'application
+	ofxPanel guiOption;
+	ofxButton exitButton;
 
+	float posX;
+	float posY;
+
+	//---------------------------------------------------------------------------------------------
+	
+	Gui();
+
+	void setup();
+	void reset();
+	void update();
+	void draw();
 
 	~Gui();
 };
